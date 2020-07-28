883373a3f76b967ed3b13382d8dd369352a12e64
Alex Braz <alexjavabraz@gmail.com>
2020-07-28 12:50:48 -0300

    dependencies

--- src/main/java/br/com/bjbraz/poll/PollApiApplication.java
+++ src/main/java/br/com/bjbraz/poll/PollApiApplication.java
@@ -2,12 +2,19 @@ package br.com.bjbraz.poll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication
+@PropertySources(value={
+		@PropertySource(value="classspath:swagger-config.properties", ignoreResourceNotFound = true)
+})
 public class PollApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PollApiApplication.class, args);
 	}
+
+
 
 }
